a4e7b49273dde2953a2d96b76c70e9d4f2de41dc
Anthony <100093373+TheOdd23@users.noreply.github.com>
2023-04-11 08:10:17 -0400

    Add files via upload

--- CPPModule03/ex00/main.cpp
+++ CPPModule03/ex00/main.cpp
@@ -1,40 +1,46 @@
 #include "ClapTrap.hpp"
 
-int	main(){
+int	main()
+{
 	ClapTrap Soupe1;
 	ClapTrap Soupe("Soupe");
-
-	Soupe1.set_name("Jean-Guy");
+    Soupe1.set_name("Jean-Guy");
 	std::cout << std::endl;
-	std::cout << Soupe1;
+	
+    std::cout << Soupe1;
 	std::cout << Soupe;
 	std::cout << std::endl;
-	Soupe1.set_attack(9000);
-	Soupe1.attack("Jesus");
+	
+    Soupe1.set_attack(9000);
+	Soupe1.attack("Beau Jack");
 	std::cout << std::endl;
-	Soupe.attack("Jesus");
+	
+    Soupe.attack("Beau Jack");
 	Soupe.takeDamage(9);
 	Soupe.beRepaired(2);
 	Soupe.beRepaired(12);
 	Soupe.set_attack(23);
-	Soupe.attack("Jesus");
-	Soupe.attack("Jesus");
+	Soupe.attack("Beau Jack");
+	Soupe.attack("Beau Jack");
 	Soupe.takeDamage(9);
 	Soupe.beRepaired(12);
-	Soupe.attack("Jesus");
+	Soupe.attack("Beau Jack");
 	std::cout << std::endl;
-	Soupe1.attack("Jesus");
+	
+    Soupe1.attack("Beau Jack");
 	std::cout << std::endl;
-	Soupe.attack("Jesus");
+	
+    Soupe.attack("Beau Jack");
 	Soupe.takeDamage(2);
 	Soupe.takeDamage(1);
 	Soupe.beRepaired(1);
 	Soupe.beRepaired(1);
 	Soupe.takeDamage(16);
-	Soupe.attack("Jesus");
+	Soupe.attack("Beau Jack");
 	std::cout << std::endl;
-	Soupe1 = Soupe;
+	
+    Soupe1 = Soupe;
 	Soupe1.beRepaired(42);
-	Soupe1.attack("Jesus");
+	Soupe1.attack("Antho");
 	return (0);
 }
--- CPPModule03/ex02/main.cpp
+++ CPPModule03/ex02/main.cpp
@@ -2,52 +2,58 @@
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
 
-int	main(){
+int	main()
+{
 	ClapTrap Soupe("Soupe");
 	ScavTrap Johny("Grelot");
 	FragTrap Poulet("FraggyBoy");
 	FragTrap Chat("Fraggy Chat");
-
 	std::cout << std::endl;
-	Poulet.highFivesGuys();
+	
+    Poulet.highFivesGuys();
 	Poulet.attack("Jerry la skag");
 	std::cout << std::endl;
-	Johny.attack("Douche Lord");
+	
+    Johny.attack("Beau Jack");
 	Johny.takeDamage(9);
 	Johny.beRepaired(256);
 	Johny.beRepaired(12);
-	Johny.attack("Douche Lord");
-	Johny.attack("Douche Lord");
+	Johny.attack("Beau Jack");
+	Johny.attack("Beau Jack");
 	Johny.takeDamage(9);
 	Johny.beRepaired(12);
 	Johny.guardGate();
 	std::cout << std::endl;
-	std::cout << "Back to ClapTrap" << std::endl;
+	
+    std::cout << "Back to ClapTrap" << std::endl;
 	std::cout << std::endl;
-	Soupe.attack("Jesus");
+	
+    Soupe.attack("Rakk");
 	Soupe.takeDamage(9);
 	Soupe.beRepaired(2);
 	Soupe.beRepaired(12);
 	Soupe.set_attack(23);
-	Soupe.attack("Jesus");
-	Soupe.attack("Jesus");
+	Soupe.attack("Rakk");
+	Soupe.attack("Rakk");
 	Soupe.takeDamage(9);
 	Soupe.beRepaired(12);
-	Soupe.attack("Jesus");
-	Soupe.attack("Jesus");
-	Soupe.attack("Jesus");
+	Soupe.attack("Rakk");
+	Soupe.attack("Rakk");
+	Soupe.attack("Rakk");
 	Soupe.takeDamage(2);
 	Soupe.takeDamage(1);
 	Soupe.beRepaired(1);
 	Soupe.beRepaired(1);
 	Soupe.takeDamage(16);
-	Soupe.attack("Jesus");
+	Soupe.attack("Rakk");
 	std::cout << std::endl;
-	std::cout << "Back to FragTraps" << std::endl;
+	
+    std::cout << "Back to FragTraps" << std::endl;
 	Chat = Poulet;
 	Poulet.attack("Brute sanguinaire");
 	std::cout << std::endl;
-	Chat.attack("Chairnier");
+	
+    Chat.attack("Chairnier");
 	Chat.highFivesGuys();
 	Chat.beRepaired(34);
 	Chat.takeDamage(12);
